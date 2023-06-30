3dc6b26914d5d5da371b50f74312115cb0750bd7
unknown <foradorif@gmail.com>
2023-06-30 11:40:53 -0300

    fix: header

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,12 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    headers: { Bearer: `WWxkV01XTkhSbnBrUjFaeldsZG9kRmxYYkhwWmJVWjVXVmhTZGc9PQ==` },
-    baseURL: 'https://marketplace-api-v2.kanal.com.br/public/api/bitrix/banker-lead'
+    headers: { 
+        Bearer: 'WWxkV01XTkhSbnBrUjFaeldsZG9kRmxYYkhwWmJVWjVXVmhTZGc9PQ==',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
+        'Access-Control-Allow-Headers': "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    },
+    baseURL: 'https://marketplace-api-v2.kanal.com.br/public/api/bitrix/banker-lead',
 })
