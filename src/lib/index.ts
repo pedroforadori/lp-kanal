162246c95fb77a88fb8b3067ff1a06daf3881437
unknown <foradorif@gmail.com>
2023-06-30 10:01:24 -0300

    fix: add Bearer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://marketplace-api-v2.kanal.com.br/public/api/bitrix/banker-lead/'
+    headers: { Bearer: `WWxkV01XTkhSbnBrUjFaeldsZG9kRmxYYkhwWmJVWjVXVmhTZGc9PQ==` },
+    baseURL: 'https://marketplace-api-v2.kanal.com.br/public/api/bitrix/banker-lead'
 })
