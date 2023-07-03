e70ee10ca291406a688df50b1b25bd5b863cebe5
pedroforadori <foradorif@gmail.com>
2023-07-03 16:30:16 -0300

    feature: add gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,10 +9,24 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Code+Latin:wght@400;500;600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://video-react.github.io/assets/video-react.css" />
         <link rel="shortcut icon" href="/assets/ico/logo-ico.ico" />
+        <script
+            dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P92PPNX');`,
+          }}
+        />
       </Head>
       <body className="font-primaryHurme">
         <Main />
         <NextScript />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P92PPNX" height="0" width="0" style="display: none; visibility: hidden;" />`,
+          }}
+        />
       </body>
     </Html>
   )
